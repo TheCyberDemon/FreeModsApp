bcb55abc0a347d4f4ad3fdf228a5ab8527dca9dc
cyraxop123 <77997274+cyraxop123@users.noreply.github.com>
2022-02-14 23:02:55 +0530

    Update cyberdemon.py

--- cyberdemon.py
+++ cyberdemon.py
@@ -17,7 +17,7 @@ path = str(os.getcwd()) + '/'
 
 def uploader():
     SESSION = requests.session()
-    FILE_NAME = os.listdir(path + "file/")[1]
+    FILE_NAME = os.listdir(path + "file/")[0]
 
     LOGIN_URL = 'https://zeus.protondns.net:2083/login/?login_only=1'
     LOGIN_HEADERS = {
